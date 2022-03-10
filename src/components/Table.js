de2a2701af96c11a6fdcb43ad88b45cbe17abd69
arminemirzoian <arminemrzoian@gmail.com>
2022-03-10 12:14:10 +0400

    virtualized table created

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import Paper from "@mui/material/Paper";
+import VirtualizedTable from "./TableItem";
+import useFetch from "../hooks/useFetch";
+import {tableUrl} from "../API/Api";
 
-import TableItem from "./TableItem";
-
-
-function Table() {
+export default function Table() {
+    const [data] = useFetch(tableUrl);
 
     return (
-        <div>
-           <TableItem/>
-        </div>
+        <>
+            {data &&
+                <Paper style={{ height: 400, width: "100%" }}>
+                    <VirtualizedTable
+                        rowCount={data.length}
+                        rowGetter={({ index }) => data[index]}
+                        columns={Object.keys(data[0]).map((cell) => ({
+                            width: 1000,
+                            label: [cell],
+                            dataKey: [cell],
+                        }))
+                    }
+
+                    />
+                </Paper>
+            }
+        </>
     );
 }
-
-export default Table;
--- src/components/TableItem.js
+++ src/components/TableItem.js
@@ -1,39 +1,127 @@
-import React, {useEffect, useState} from 'react';
-import MaterialTable from "material-table";
-import {getTable} from "../API/tableAPI";
+import * as React from "react";
+import { AutoSizer, Column, Table } from "react-virtualized";
+import clsx from "clsx";
+import { withStyles } from "@mui/styles";
+import { createTheme } from "@mui/material/styles";
+import TableCell from "@mui/material/TableCell";
 
-function TableItem() {
-    const [table, setTable] = useState([]);
-    useEffect(() => {
-        getTable().then(res => {
-            console.log(res)
-            setTable(res.data)
-        })
-            .catch(err => console.log(err))
-    }, [])
 
-    const columns = [
-        {title: 'Disclosure_year', field: 'disclosure_year'},
-        {title: 'Disclosure_date', field: 'disclosure_date'},
-        {title: 'Transaction_date', field: 'transaction_date'},
-        {title: 'Owner', field: 'owner'},
-        {title: 'Asset_description', field: 'asset_description'},
-        {title: 'Type', field: 'type'},
-        {title: 'Amount', field: 'amount'},
-        {title: 'Representative', field: 'representative'},
-        {title: 'District', field: 'district'},
-        {title: 'Ptr_link', field: 'ptr_link'},
-        {title: 'Cap_gains_over_200_usd', field: 'cap_gains_over_200_usd'},
-    ]
+const styles = (theme) => ({
+    flexContainer: {
+        display: "flex",
+        alignItems: "center",
+        boxSizing: "border-box",
+    },
+    table: {
+        "& .ReactVirtualized__Table__headerRow": {
+            ...(theme.direction === "rtl" && {
+                paddingLeft: "0 !important",
+            }),
+            ...(theme.direction !== "rtl" && {
+                paddingRight: undefined,
+            }),
+        },
+    },
+    tableRowHover: {
+        "&:hover": {
+            backgroundColor: theme.palette.grey[200],
+        },
+    },
+    tableCell: {
+        flex: 1,
+    },
+    noClick: {
+        cursor: "initial",
+    },
+});
+
+function MuiVirtualizedTable({headerHeight= 48,
+                                 rowHeight = 48, classes, columns, ...tableProps}) {
+
+    const getRowClassName = ({ index }) => {
+        return clsx(classes.tableRow, classes.flexContainer, {
+            [classes.tableRowHover]: index !== -1
+        });
+    };
+
+    const cellRenderer = ({ cellData, columnIndex }) => {
+        return (
+            <TableCell
+                component="div"
+                className={clsx(classes.tableCell, classes.flexContainer, {
+                })}
+                variant="body"
+                style={{ height: rowHeight }}
+                align={
+                    (columnIndex != null && columns[columnIndex].numeric) || false
+                        ? "right"
+                        : "left"
+                }
+            >
+                {cellData}
+            </TableCell>
+        );
+    };
+
+    const headerRenderer = ({ label, columnIndex }) => {
+
+        return (
+            <TableCell
+                component="div"
+                className={clsx(
+                    classes.tableCell,
+                    classes.flexContainer,
+                )}
+                variant="head"
+                style={{ height: headerHeight }}
+                align={columns[columnIndex].numeric || false ? "right" : "left"}
+            >
+                <span>{label}</span>
+            </TableCell>
+        );
+    };
 
     return (
-        <div>
-            <MaterialTable title="Material table" columns={columns} data={table} options={{search: false, pageSize: 10, paging: false,  toolbar: false,
-                thirdSortClick: false, maxBodyHeight: 400,
-                headerStyle: { position: 'sticky', top: 0 },}} />
-
-        </div>
+        <AutoSizer>
+            {({ height, width }) => (
+                <Table
+                    height={height}
+                    width={width}
+                    rowHeight={rowHeight}
+                    gridStyle={{
+                        direction: "inherit",
+                    }}
+                    headerHeight={headerHeight}
+                    className={classes.table}
+                    {...tableProps}
+                    rowClassName={getRowClassName}
+                >
+                    {columns.map(({ dataKey, ...other }, index) => {
+                        return (
+                            <Column
+                                key={dataKey}
+                                headerRenderer={(headerProps) =>
+                                    headerRenderer({
+                                        ...headerProps,
+                                        columnIndex: index,
+                                    })
+                                }
+                                className={classes.flexContainer}
+                                cellRenderer={cellRenderer}
+                                dataKey={dataKey}
+                                {...other}
+                            />
+                        );
+                    })}
+                </Table>
+            )}
+        </AutoSizer>
     );
 }
 
-export default TableItem
+const defaultTheme = createTheme();
+const VirtualizedTable = withStyles(styles, { defaultTheme })(
+    MuiVirtualizedTable
+);
+
+export default VirtualizedTable
